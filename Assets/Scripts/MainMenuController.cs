321bf624d43ddfc1be73ba65e0fcd8ed68d1d806
Kumuditha Perera <kumudithaperera@gmail.com>
2020-02-02 06:16:04 +0530

    sounds and animations

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+public void Select (string levelName){
+
+}
+}
